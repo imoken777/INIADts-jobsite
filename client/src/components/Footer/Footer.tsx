706ba640efc3c7f964cbd814e2b88f6608bf7fc1
imoken777 <imoken89@gmail.com>
2023-08-13 15:23:27 +0900

    細部デザインの変更

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -11,17 +11,16 @@ export const Footer = () => {
         <div className={styles.tabItem}>OUTPUT</div>
         <div className={styles.tabItem}>DEBUG CONSOLE</div>
         <div className={`${styles.tabItem} ${styles.choice}`}>TERMINAL</div>
-        <div className={styles.tabItem}>PORTS</div>
       </div>
       <div className={styles.body}>
         <p className={styles.p}>
           <Circle />
-          PS C:\Toyo\INIAD\INIAD.ts{'>'} <span className={styles.git}>git</span> contact -v
+          PS C:\Toyo\INIAD\INIAD.ts{'>'} <span className={styles.git}>cat</span> contact.ts
         </p>
         <p className={styles.p}>INIAD.ts メールアドレスなどを張る</p>
         <p className={styles.p}>
           <Circle />
-          PS C:\Toyo\INIAD\INIAD.ts{'>'} <span className={styles.git}>git</span> copyright -v
+          PS C:\Toyo\INIAD\INIAD.ts{'>'} <span className={styles.git}>echo</span> copyright
         </p>
         <p className={styles.p}>© 2023 INIAD.ts</p>
       </div>
